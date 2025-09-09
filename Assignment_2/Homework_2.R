1790c6f1e88b5786df0978abbbc00881d35e7038
Alessandra Serrano <allys.bec@gmail.com>
2025-09-09 09:39:19 -0400

    Added Homework_2.R file

---
+++ Assignment_2/Homework_2.R
@@ -0,0 +1,5 @@
+library(tidyverse) #load the tidyverse package
+setwd("/Users/alessandraserrano/Documents/Classes/Fall_2025/BIOSTAT 521/Chapter2_Summarizing_Data/Assignment_2/")
+
+DATA = read_csv("bivariate_data.csv")
+view(DATA)
